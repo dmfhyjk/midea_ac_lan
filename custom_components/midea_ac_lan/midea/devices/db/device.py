1121ac2f979214c55da9ee581a8e405ef4f1d50e
dmfhyjk <52057811+dmfhyjk@users.noreply.github.com>
2023-06-04 20:12:46 +0800

    功能更新
    增加洗涤参数读取功能，提供几种模式选择（标准洗涤、30‘快速洗涤、桶自洁等，但无法修改详细时间），完善了一些注释

--- custom_components/midea_ac_lan/midea/devices/db/device.py
+++ custom_components/midea_ac_lan/midea/devices/db/device.py
@@ -3,23 +3,125 @@ from .message import (
     MessageQuery,
     MessagePower,
     MessageStart,
-    MessageDBResponse
+    MessageSet,
+    MessageDBResponse,
+    water_dict,
+    rinsing_dict,
+    temp_dict,
+    speed_dict,
+    detergent_dict,
+    softener_dict,
+    mode_dict
 )
 from ...core.device import MiedaDevice
 from ...backports.enum import StrEnum
 
 _LOGGER = logging.getLogger(__name__)
-
-
+
 class DeviceAttributes(StrEnum):
     power = "power"
     start = "start"
-    washing_data = "washing_data"
-    progress = "progress"
     time_remaining = "time_remaining"
-
-
+    mode = "mode"
+    preset_mode = "preset_mode"
+    progress = "progress"
+    washing_data = "washing_data"
+    water_level = "water_level"
+#    washing_level = "washing_level"
+#    dehydration_level = "dehydration_level"
+    temperature = "temperature"
+    washing_time = "washing_time"
+    rinsing = "rinsing"
+    speed = "speed"
+    dehydration_time = "dehydration_time"
+    detergent = "detergent"
+    softener = "softener"
+    nanobubbles = "nanobubbles"
+    uv = "uv"
+    
 class MideaDBDevice(MiedaDevice):
+
+    _water_dict = water_dict
+    
+    _rinsing_dict = rinsing_dict
+    
+    _temp_dict = temp_dict
+    
+    _speed_dict = speed_dict
+    
+    _detergent_dict = detergent_dict
+    
+    _softener_dict = softener_dict
+
+    _mode_dict = mode_dict
+
+    _preset_mode_dict = {
+	    0x1f: "标准洗",
+	    0x17: "30'快洗",
+	    0x09: "单脱水",
+	    0x0a: "漂脱",
+	    0x0b: "大件洗",
+	    0x64: "桶自洁",
+	    0x1a: "浸泡洗",
+}
+
+#    preset_washing_data = {
+#
+#        "标准洗": bytearray([
+#            0xff, 0xff, 0x00, 0x1f, 0x05, 
+#            0x20, 0x03, 0x03, 0x00, 0x00, 
+#            0x04, 0x01, 0x00, 0x00, 0x00
+#        ]),
+#        "30'快洗": bytearray([
+#            0xff, 0xff, 0x00, 0x17, 0x01, 
+#            0x20, 0x03, 0x03, 0x00, 0x00, 
+#            0x04, 0x01, 0x00, 0x00, 0x00
+#        ]),
+#        "单脱水": bytearray([
+#            0xff, 0xff, 0x00, 0x09, 0x00, 
+#            0x00, 0xff, 0x03, 0x00, 0x03, 
+#            0x00, 0x00, 0x00, 0x00, 0x00
+#        ]),
+#        "漂脱": bytearray([
+#            0xff, 0xff, 0x00, 0x0a, 0x01, 
+#            0x20, 0xff, 0x00, 0x00, 0x00, 
+#            0x00, 0x01, 0x00, 0x00, 0x00
+#        ]),
+#        "大件洗": bytearray([
+#            0xff, 0xff, 0x00, 0x0b, 0x01, 
+#            0x20, 0x04, 0x03, 0x00, 0x00, 
+#            0x04, 0x01, 0x00, 0x00, 0x00
+#        ]),
+#        "桶自洁": bytearray([
+#            0xff, 0xff, 0x00, 0x64, 0x00, 
+#            0x10, 0x06, 0x00, 0x00, 0x00, 
+#            0x00, 0x00, 0x00, 0x00, 0x00
+#        ]),
+#        "浸泡洗": bytearray([
+#            0xff, 0xff, 0x00, 0x1a, 0x05, 
+#            0x20, 0x02, 0x03, 0x00, 0x00, 
+#            0x04, 0x01, 0x00, 0x00, 0x00
+#        ])
+#
+#    }
+
+#    _washing_dict = {
+#        0x04: "1档", 
+#        0x00: "2档", 
+#        0x01: "3档",
+#        0x02: "4档",
+#        0x03: "5档", 
+#        
+#    }
+#
+#    _dehydration_dict = {
+#        0x03: "1档", 
+#        0x04: "2档",
+#        0x00: "3档",
+#        0x01: "4档", 
+#        0x02: "5档", 
+#    }
+    
     def __init__(
             self,
             name: str,
@@ -47,9 +149,40 @@ class MideaDBDevice(MiedaDevice):
             DeviceAttributes.power: False,
             DeviceAttributes.start: False,
             DeviceAttributes.washing_data: bytearray([]),
+            DeviceAttributes.mode: 0x1f,
             DeviceAttributes.progress: "Unknown",
-            DeviceAttributes.time_remaining: None
+            DeviceAttributes.time_remaining: None,
+            DeviceAttributes.water_level: "自动",
+            DeviceAttributes.preset_mode: "30'快洗",
+#            DeviceAttributes.washing_level: "2档",
+#            DeviceAttributes.dehydration_level: "3档",
+            DeviceAttributes.temperature: "None",
+            DeviceAttributes.washing_time: "None",
+            DeviceAttributes.dehydration_time: "None",
+            DeviceAttributes.rinsing: "None",
+            DeviceAttributes.speed: "800",
+            DeviceAttributes.dehydration_time: "None",
+            DeviceAttributes.detergent: "自动",
+            DeviceAttributes.softener: "自动",
+            DeviceAttributes.nanobubbles: True,
+            DeviceAttributes.uv: True,
         }
+        
+#    @property
+#    def water_level_sets(self):
+#        return list(MideaDBDevice._water_dict.values())
+#
+#    @property
+#    def washing_level_sets(self):
+#        return list(MideaDBDevice._washing_dict.values())
+#
+#    @property
+#    def dehydration_level_sets(self):
+#        return list(MideaDBDevice._dehydration_dict.values())
+
+    @property
+    def preset_mode_sets(self):
+        return list(MideaDBDevice._preset_mode_dict.values())
 
     def build_query(self):
         return [MessageQuery(self._device_protocol_version)]
@@ -58,8 +191,8 @@ class MideaDBDevice(MiedaDevice):
         message = MessageDBResponse(msg)
         _LOGGER.debug(f"[{self.device_id}] Received: {message}")
         new_status = {}
-        progress = ["Idle", "Spin", "Rinse", "Wash", "Pre-wash",
-                    "Dry", "Weight", "Hi-speed Spin", "Unknown"]
+        progress = ["待机", "脱水", "漂洗", "洗涤", "预洗",
+                    "干燥", "称重", "高速脱水", "未知"]
         for status in self._attributes.keys():
             if hasattr(message, status.value):
                 if status == DeviceAttributes.progress:
@@ -67,7 +200,41 @@ class MideaDBDevice(MiedaDevice):
                 else:
                     self._attributes[status] = getattr(message, status.value)
                 new_status[status.value] = self._attributes[status]
+        
         return new_status
+
+    def make_message_set(self):
+        message = MessageSet(self._device_protocol_version)
+
+#        message.mode = 0x17 if self._attributes[DeviceAttributes.mode] is None else \
+#            list(MideaDBDevice._mode_dict.keys())[list(MideaDBDevice._mode_dict.values()).index(
+#                self._attributes[DeviceAttributes.mode]
+#            )]
+        # message.mode = 0x02
+
+        # message.temperature = self._attributes[DeviceAttributes.temperature]
+              # 温度
+        # message.rinsing = self._attributes[DeviceAttributes.rinsing]
+                # 漂洗次数
+        # message.speed = self._attributes[DeviceAttributes.speed]
+                    # 脱水转速挡位
+        # message.detergent = self._attributes[DeviceAttributes.detergent]
+             # 洗涤剂
+        # message.softener = self._attributes[DeviceAttributes.softener]
+                # 柔顺剂
+#        message.water_level = 0x05 if self._attributes[DeviceAttributes.water_level] is None else \
+#            list(MideaDBDevice._water_dict.keys())[list(MideaDBDevice._water_dict.values()).index(
+#                self._attributes[DeviceAttributes.water_level]
+#            )]
+    
+        # message.washing_level = self._attributes[DeviceAttributes.washing_level]
+            # 洗涤挡位
+        # message.dehydration_level = self._attributes[DeviceAttributes.dehydration_level]
+        # 脱水挡位
+
+#        _LOGGER.debug(f"[{self.device_id}] build message: {message}")
+
+        return message
 
     def set_attribute(self, attr, value):
         if attr == DeviceAttributes.power:
@@ -80,6 +247,29 @@ class MideaDBDevice(MiedaDevice):
             message.washing_data = self._attributes[DeviceAttributes.washing_data]
             self.build_send(message)
 
+        elif attr == DeviceAttributes.preset_mode:
+            message = MessageSet(self._device_protocol_version, preset_mode=value)
+#            message = self.make_message_set()
+#            message.washing_data = self.preset_washing_data[value]
+            _LOGGER.debug(f"[{self.device_id}] value: {value} build message: {message}")
+
+#            if attr == DeviceAttributes.preset_mode:
+#                pass
+#                if value in MideaDBDevice._water_dict.values():
+#                    message.water_level = list(MideaDBDevice._water_dict.keys())[
+#                        list(MideaDBDevice._water_dict.values()).index(value)
+#                    ]
+#            if attr == DeviceAttributes.washing_level:
+#                if value in MideaDBDevice._washing_dict.values():
+#                    message.washing_level = list(MideaDBDevice._washing_dict.keys())[
+#                        list(MideaDBDevice._washing_dict.values()).index(value)
+#                    ]
+#            if attr == DeviceAttributes.dehydration_level:
+#                if value in MideaDBDevice._dehydration_dict.values():
+#                    message.dehydration_level = list(MideaDBDevice._dehydration_dict.keys())[
+#                        list(MideaDBDevice._dehydration_dict.values()).index(value)
+#                    ]
+            self.build_send(message)
 
 class MideaAppliance(MideaDBDevice):
     pass
--- custom_components/midea_ac_lan/midea/devices/db/message.py
+++ custom_components/midea_ac_lan/midea/devices/db/message.py
@@ -5,6 +5,132 @@ from ...core.message import (
     MessageBody,
 )
 
+water_dict = {
+    
+    0x01: "1",
+    0x02: "2",
+    0x03: "3",
+    0x04: "4",
+    0x05: "自动",
+}
+
+rinsing_dict = {
+    
+    0x00: "0",
+    0x10: "1",
+    0x20: "2",
+    0x30: "3",
+    0x40: "4",
+}
+
+temp_dict = {
+    0x01: "冷水",
+    0x02: "20", 
+    0x03: "30", 
+    0x04: "40", 
+    0x05: "50", 
+    0x06: "60", 
+    0x07: "70", 
+    0xff: None, 
+}
+
+speed_dict = {
+    0x00: "None", 
+    0x01: "400",
+    0x02: "600",
+    0x03: "800", 
+    0x04: "1000", 
+    0x05: "1200", 
+}
+
+detergent_dict = {
+    0x00: "None", 
+    0x01: "1",
+    0x02: "2",
+    0x03: "3", 
+    0x04: "自动", 
+    0x05: "4", 
+}
+
+softener_dict = {
+    0x00: "None", 
+    0x01: "自动",
+    0x02: "1",
+    0x03: "2", 
+    0x04: "3", 
+    0x05: "4", 
+}
+
+#washing_dict = {
+#   0x04: "1档", 
+#   0x00: "2档", 
+#   0x01: "3档",
+#   0x02: "4档",
+#   0x03: "5档", 
+#   
+#}
+#
+#dehydration_dict = {
+#   0x03: "1档", 
+#   0x04: "2档",
+#   0x00: "3档",
+#   0x01: "4档", 
+#   0x02: "5档", 
+#}
+
+mode_dict = {
+    0x1f: "标准洗",
+    0x17: "30'快洗",
+    0x09: "单脱水",
+    0x0a: "漂脱",
+    0x0b: "大件洗",
+    0x64: "桶自洁",
+    0x1a: "浸泡洗",
+    0x05: "羊毛洗",
+    0x63: "除螨洗",
+    0x02: "15'快洗",
+    0xff: "未知"
+}
+
+preset_washing_data = {
+
+   "标准洗": bytearray([
+       0xff, 0xff, 0x00, 0x1f, 0x05, 
+       0x20, 0x03, 0x03, 0x00, 0x00, 
+       0x04, 0x01, 0x00, 0x00, 0x00
+   ]),
+   "30'快洗": bytearray([
+       0xff, 0xff, 0x00, 0x17, 0x01, 
+       0x20, 0x03, 0x03, 0x00, 0x00, 
+       0x04, 0x01, 0x00, 0x00, 0x00
+   ]),
+   "单脱水": bytearray([
+       0xff, 0xff, 0x00, 0x09, 0x00, 
+       0x00, 0xff, 0x03, 0x00, 0x03, 
+       0x00, 0x00, 0x00, 0x00, 0x00
+   ]),
+   "漂脱": bytearray([
+       0xff, 0xff, 0x00, 0x0a, 0x01, 
+       0x20, 0xff, 0x00, 0x00, 0x00, 
+       0x00, 0x01, 0x00, 0x00, 0x00
+   ]),
+   "大件洗": bytearray([
+       0xff, 0xff, 0x00, 0x0b, 0x01, 
+       0x20, 0x04, 0x03, 0x00, 0x00, 
+       0x04, 0x01, 0x00, 0x00, 0x00
+   ]),
+   "桶自洁": bytearray([
+       0xff, 0xff, 0x00, 0x64, 0x00, 
+       0x10, 0x06, 0x00, 0x00, 0x00, 
+       0x00, 0x00, 0x00, 0x00, 0x00
+   ]),
+   "浸泡洗": bytearray([
+       0xff, 0xff, 0x00, 0x1a, 0x05, 
+       0x20, 0x02, 0x03, 0x00, 0x00, 
+       0x04, 0x01, 0x00, 0x00, 0x00
+   ])
+
+}
 
 class MessageDBBase(MessageRequest):
     def __init__(self, device_protocol_version, message_type, body_type):
@@ -45,13 +171,65 @@ class MessagePower(MessageDBBase):
         power = 0x01 if self.power else 0x00
         return bytearray([
             power,
-            0xFF, 0xFF, 0xFF, 0xFF,
-            0xFF, 0xFF, 0xFF, 0xFF,
-            0xFF, 0xFF, 0xFF, 0xFF,
-            0xFF, 0xFF, 0xFF, 0xFF,
-            0xFF, 0xFF, 0xFF, 0xFF
+            # 0x00, 0x00, 0x17,
+            # 0x01, 0x20, 0x01, 0x03,
+            # 0x00, 0x00, 0x04, 0x01,
+            # 0x00, 0x00, 0x00
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff
         ])
 
+class MessageSet(MessageDBBase):
+    def __init__(self, device_protocol_version, preset_mode="30'快洗"):
+        super().__init__(
+            device_protocol_version=device_protocol_version,
+            message_type=MessageType.set,
+            body_type=0x02)
+
+#        self.mode = 0x17
+#        self.water_level = 0x40            # 水量
+#        self.temperature = 0x01              # 温度
+#        self.rinsing = 0x20                # 漂洗次数
+#        self.speed = 0x03                    # 脱水转速挡位
+#        self.detergent = 0x04             # 洗涤剂
+#        self.softener = 0x01                # 柔顺剂
+#        self.water_level = 0x05            # 水量挡位
+#        self.washing_level = 0x00            # 洗涤挡位
+#        self.dehydration_level = 0x00        # 脱水挡位
+        self.preset_mode = preset_mode        # 脱水挡位
+
+    @property
+    def _body(self):
+        # byte2 mode
+#        mode = self.mode if self.mode else 0x17
+#
+#        water_level = self.water_level if self.water_level else 0x40
+#        washing_level = self.washing_level if self.washing_level else 0x00
+#        dehydration_level = self.dehydration_level if self.dehydration_level else 0x00
+#        temperature = self.temperature if self.temperature else 0x01
+#        rinsing = self.rinsing if self.rinsing else 0x02
+#        speed = self.speed if self.speed else 0x03
+#        detergent = self.detergent if self.detergent else 0x04
+#        softener = self.softener if self.softener else 0x01
+#        return bytearray([
+#            0xff,            # 电源
+#            0xff,            # 运行状态
+#            0x00,            # 空
+#            mode,             # 洗涤模式
+#            water_level,     # 水量
+#            rinsing,         # 漂洗次数
+#            temperature,     # 洗涤温度
+#            speed,             # 脱水转速
+#            washing_level,         # 洗涤等级
+#            dehydration_level,     # 脱水等级
+#            detergent,             # 洗涤剂
+#            softener,            # 柔顺剂
+#            0x00, 0x00, 0x00
+#        ])
+        return preset_washing_data[self.preset_mode]
 
 class MessageStart(MessageDBBase):
     def __init__(self, device_protocol_version):
@@ -66,8 +244,14 @@ class MessageStart(MessageDBBase):
     def _body(self):
         if self.start: # Pause
             return bytearray([
-                0xFF, 0x01
-            ]) + self.washing_data
+                0xFF, 0x01,
+            # ]) + self.washing_data
+
+                0x00, 0x17, 0x01, 0x20, 
+                0x03, 0x03, 0x00, 0x00, 
+                0x04, 0x01, 0x00, 0x00, 
+                0x00
+            ])
         else:
             # Pause
             return bytearray([
@@ -78,18 +262,37 @@ class MessageStart(MessageDBBase):
 class DBGeneralMessageBody(MessageBody):
     def __init__(self, body):
         super().__init__(body)
-        self.power = body[1] > 0
-        self.start = True if body[2] in [2, 6] else False
+        self.power = body[1] > 0                                                                   # 电源
+        self.start = True if body[2] in [2, 6] else False                                          # 运行状态
         self.washing_data = body[3:16]
+              
+        self.mode = mode_dict[body[4]] if body[4] in mode_dict.keys() else None                     # 洗涤模式
+        self.preset_mode = mode_dict[body[4]] if body[4] in mode_dict.keys() else None
+        self.water_level = water_dict[body[5]] if body[5] in water_dict.keys() else None            # 水量
+        self.temperature = temp_dict[body[7]] if body[7] in temp_dict.keys() else None              # 温度
+        self.washing_time = body[27] if self.power else None                                        # 洗涤时间
+        self.rinsing = rinsing_dict[body[6]] if body[6] in rinsing_dict.keys() else None            # 漂洗次数
+        self.speed = speed_dict[body[8]] if body[8] in speed_dict.keys() else None                    # 脱水转速挡位
+#        self.washing_level = washing_dict[body[9]] if body[9] in washing_dict.keys() else None                # 洗涤时长等级
+#        self.dehydration_level = dehydration_dict[body[10]] if body[10] in dehydration_dict.keys() else None                # 脱水时长等级
+        self.washing_level = body[9]                # 洗涤时长等级
+        self.dehydration_level = body[10]                # 脱水时长等级
+        self.dehydration_time = body[28] if self.power else None                                   # 脱水时间
+        self.detergent = detergent_dict[body[11]] if body[11] in detergent_dict.keys() else None     # 洗涤剂
+        self.softener = softener_dict[body[12]] if body[12] in softener_dict.keys() else None        # 柔顺剂
+        self.nanobubbles = True if (body[29] & 0x20) else False                      # 超微净泡
+        self.uv = True if (body[29] & 0x08) else False                             # uv除菌
+        self.time_remaining = (body[17] + (body[18] << 8)) if self.power else None     # 剩余时间
+
         self.progress = 0
-        for i in range(0, 7):
+        for i in range(0, 7):            # 当前步骤
             if (body[16] & (1 << i)) > 0:
                 self.progress = i + 1
                 break
-        if self.power:
-            self.time_remaining = body[17] + (body[18] << 8)
-        else:
-            self.time_remaining = None
+        # if self.power:
+        #     self.time_remaining = body[17] + (body[18] << 8)    # 剩余时间
+        # else:
+        #     self.time_remaining = None
 
 
 class MessageDBResponse(MessageResponse):
